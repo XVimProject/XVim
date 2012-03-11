bcd2c9a1663ee8034783288f9757901de0162e38
Shuichiro Suzuki <shu@jugglershu.net>
2012-03-11 18:48:25 +0900

    Fixed Delete evaluators following bug.  - 'dd' on blankline make delete 2 lines.  - 'dd' on a blankline at the end of document crashes

--- XVim/NSTextView+VimMotion.h
+++ XVim/NSTextView+VimMotion.h
@@ -13,6 +13,14 @@ typedef enum{
     LEFT_RIGHT_NOWRAP
 } MOTION_OPTION;
 
+BOOL isDigit(unichar ch);
+BOOL isAlpha(unichar ch);
+BOOL isDelimeter(unichar ch);
+BOOL isWhiteSpace(unichar ch);
+BOOL isNonAscii(unichar ch);
+BOOL isNewLine(unichar ch);
+BOOL isFuzzyWord(unichar ch);
+
 @interface NSTextView (VimMotion)
 - (NSUInteger)nextNewline;
 - (NSUInteger)prevNewline;
