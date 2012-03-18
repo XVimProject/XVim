1e8c533bc23a718bc5ab772f609891119615d3a4
dojoteef <dojoteef@gmail.com>
2012-03-18 15:38:07 -0700

    Fix asserts and incorrect behavior for '=='

--- XVim/XVimEqualEvaluator.m
+++ XVim/XVimEqualEvaluator.m
@@ -27,30 +27,35 @@
 - (XVimEvaluator*)EQUAL:(id)arg{
     NSTextView *view = [self textView];
     NSRange begin = [view selectedRange];
-    [view moveToBeginningOfLine:self];
-    NSRange start = [view selectedRange];
-    for (int i = 1; i < _repeat; i++) {
-        [view moveDown:self];
+
+    NSUInteger from = [view headOfLine:begin.location];
+    if (from == NSNotFound){
+        from = begin.location;
+    }
+
+    NSUInteger to = from;
+    for (int i = 1; i < _repeat; ++i) {
+        NSUInteger next = [view nextNewLine:to];
+        if (next == NSNotFound){
+            [self.xvim ringBell];
+            break;
+        }
+        to = next + 1;
     }
     
-    [view moveToEndOfLine:self];
-    [view moveForward:self]; // include eol
-    
-    NSRange end = [view selectedRange];
-    [view setSelectedRange:NSMakeRange(begin.location, end.location - begin.location)];
-    
-    [[view textStorage] indentCharacterRange: [view selectedRange] undoManager:[view undoManager]];
-    
-    // set cursor back to original position
-    [view setSelectedRange:begin];
-    
-    NSUInteger max = [[view string] length] - 1;
-    return [self _motionFixedFrom:start.location To:end.location>max?max:end.location Type:LINEWISE];
+    NSUInteger next = [view endOfLine:to];
+    if (next != NSNotFound){
+        to = next + 1;
+    }
+    TRACE_LOG(@"to: %d", to);
+    return [self motionFixedFrom:from To:to Type:LINEWISE];
 }
 
 -(XVimEvaluator*)motionFixedFrom:(NSUInteger)from To:(NSUInteger)to Type:(MOTION_TYPE)type{
     TRACE_LOG(@"from:%d to:%d type:%d", from, to, type);
     NSTextView* view = [self textView];
+    NSRange begin = [view selectedRange];
+
     if( from > to ){
         NSUInteger tmp = from;
         from = to;
@@ -60,14 +65,14 @@
         to--;
     }else if( type == LINEWISE ){
         [view setSelectedRange:NSMakeRange(to,0)];
-        to = [view nextNewline];
+        to = [view nextNewLine:to];
         if( NSNotFound == to ){
             to = [view string].length-1;
         }
         
         NSRange r = [view selectedRange];
         [view setSelectedRange:NSMakeRange(from,0)];
-        from = [view prevNewline];
+        from = [view prevNewLine:from];
         [view setSelectedRange:r];
         if( NSNotFound == from ){
             from = 0;
@@ -75,14 +80,13 @@
             from++;
         }
     }
-    
-    NSRange begin = [view selectedRange];
+
     [view setSelectedRangeWithBoundsCheck:from To:to];
     NSUInteger currentLength = [[view string] length] - 1;
-    
+
     // Indent
     [[view textStorage] indentCharacterRange: [view selectedRange] undoManager:[view undoManager]];
-    
+
     begin.location -= currentLength - [[view string] length] + 1;
     [view setSelectedRange:begin];
     return nil;
