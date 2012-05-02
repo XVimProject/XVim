f0d02b2bc90862f04a0f36f996aee7711123be49
Shuichiro Suzuki <shu@jugglershu.net>
2012-05-03 03:25:24 +0900

    Add script to dump header file ( not complete )

---
+++ Tools/xcodedump.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+XcodePath = "/Developer/Applications/Xcode.app"
+LibraryPaths = ["/Contents/Frameworks/IDEFoundation.framework",
+				"/Contents/Frameworks/IDEKit.framework",
+				"/Contents/SharedFrameworks/DVTFoundation.framework",
+				"/Contents/SharedFrameworks/DVTKit.framework",
+				"/Contents/PlugIns/IDESourceEditor.ideplugin"
+				]
+OutDir = "XcodeClasses/"
+
+LibraryPaths.each{|file|
+	output = OutDir + File.basename(file, ".*")
+	puts `class-dump -C "DVT.*|IDE.*" #{XcodePath}#{file} > #{output}.h`
+}
