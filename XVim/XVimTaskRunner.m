e37409ebf9e48571ca843dfa25c6f3d4d1cf45fe
Ant <ant@dervishsoftware.com>
2012-09-02 22:53:36 +0100

    Expand tildes, catch exceptions, propagate environment

--- XVim/XVimTaskRunner.m
+++ XVim/XVimTaskRunner.m
@@ -35,66 +35,115 @@
 
 
 @implementation XVimTaskRunner
+
++(BOOL)_waitForTaskToTerminate:(NSTask*)task
+{
+    NSUInteger waitCount = 0;
+    BOOL taskKilled = NO;
+    
+    while ([task isRunning] && waitCount < 7) {
+        waitCount++;
+        [ NSThread sleepForTimeInterval:0.25 ];
+    }
+    if ([task isRunning]) {
+        [task terminate];
+        taskKilled = YES;
+    }
+    return taskKilled;
+}
+
 +(NSString*) runScript:(NSString*)scriptAndArgs withInput:(NSString*)input
 {
     NSString* returnString = nil;
     NSTask *task = AUTORELEASE([[NSTask alloc] init]);
-    NSString* shellPath = @"/bin/sh";
-    [task setLaunchPath: shellPath ];
+    if (!scriptAndArgs || [ scriptAndArgs length ] == 0) {
+        return nil;
+    }
 
-    NSArray *arguments = [ NSArray arrayWithObjects:@"-c",scriptAndArgs, nil];
-    [task setArguments: arguments];
-    NSString* command = [NSString stringWithFormat:@"%@ %@", shellPath, [ arguments componentsJoinedByString:@" "]];
+    NSArray* unexpandedCommandAndArgs = [ scriptAndArgs componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    NSMutableArray* commandAndArgs = [NSMutableArray arrayWithCapacity:[unexpandedCommandAndArgs count]];
+    [ unexpandedCommandAndArgs enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [commandAndArgs addObject:[(NSString*)obj stringByExpandingTildeInPath]];
+    } ];
     
+    NSString* command = [ [commandAndArgs objectAtIndex:0] stringByExpandingTildeInPath ];
+    
+    [task setLaunchPath: command];
+
+    if ( [ commandAndArgs count ] > 1)
+    {
+        NSArray *arguments = [ commandAndArgs subarrayWithRange:NSMakeRange(1, ([commandAndArgs count] - 1))];
+        [task setArguments: arguments];
+    }
     NSPipe *outputPipe = [NSPipe pipe];
     [task setStandardOutput: outputPipe];
     NSPipe *inputPipe = [NSPipe pipe];
     [task setStandardInput:inputPipe];
+    NSPipe *errorPipe = [NSPipe pipe];
+    [task setStandardError:errorPipe];
     
     NSFileHandle *inputFile = nil;
-    NSFileHandle *outputFile = nil;
-    outputFile = [outputPipe fileHandleForReading];
+    NSFileHandle *outputFile = [outputPipe fileHandleForReading];
+    NSFileHandle *errorFile = [errorPipe fileHandleForReading];
     if (input!=nil)
     {
         inputFile = [inputPipe fileHandleForWriting];
     }
     
-    [task launch];
-    
-    if ([ task isRunning])
-    {
-        int pid = [task processIdentifier];
-        DEBUG_LOG(@"Running Task PID = %d, command = %@", pid, command);
+    NSMutableDictionary *environment = [NSMutableDictionary dictionary];
+	[environment addEntriesFromDictionary:[[NSProcessInfo processInfo] environment]];
+	[environment addEntriesFromDictionary:[task environment]];
+	[environment setObject:@"YES" forKey:@"NSUnbufferedIO"];
+	[task setEnvironment:environment];
+
+    @try {
+        [task launch];
         
-        if (input!=nil)
+        if ([ task isRunning])
         {
-            NSData *inputData = [ input dataUsingEncoding:NSUTF8StringEncoding ];
-            [ inputFile writeData:inputData ];
-            [ inputFile closeFile ];
+            int pid = [task processIdentifier];
+            DEBUG_LOG(@"Running Task PID = %d, command = %@", pid, scriptAndArgs);
+            
+            if (input!=nil)
+            {
+                NSData *inputData = [ input dataUsingEncoding:NSUTF8StringEncoding ];
+                [ inputFile writeData:inputData ];
+                [ inputFile closeFile ];
+            }
+            
+            NSData *outputData = [outputFile readDataToEndOfFile];
+            BOOL taskKilled = [ self _waitForTaskToTerminate:task ] ;
+            
+            if (!taskKilled)
+            {
+                int exitStatus = [ task terminationStatus ];
+                
+                if ( exitStatus == 0 )
+                {
+                    returnString = AUTORELEASE([[NSString alloc] initWithData: outputData encoding: NSUTF8StringEncoding]);
+                    DEBUG_LOG(@"Command returned status %d. PID = %d, Command = %@, Stdout = %@", exitStatus, pid, scriptAndArgs, returnString );
+                    
+                    
+                }
+                else
+                {
+                    NSData* errorData = [ errorFile readDataOfLength:1024 ];
+                    NSString* errorString = AUTORELEASE([[NSString alloc] initWithData:errorData encoding:NSUTF8StringEncoding]);
+                    ERROR_LOG(@"Command returned non-zero status %d. PID = %d, Command = %@, Stderr = %@", exitStatus, pid, scriptAndArgs, errorString );
+                }
+            }
+            else
+            {
+                ERROR_LOG(@"Had to kill task that refused to exit. PID = %d, Command = %@", pid, scriptAndArgs );
+            }
         }
-        NSData *outputData = [outputFile readDataToEndOfFile];
-        
-        NSUInteger waitCount = 0;
-        BOOL taskKilled = NO;
-        
-        while ([task isRunning] && waitCount < 7) {
-            waitCount++;
-            [ NSThread sleepForTimeInterval:0.25 ];
-        }
-        if ([task isRunning]) {
-            [task terminate];
-            taskKilled = YES;
-            ERROR_LOG(@"Had to kill task that refused to exit. PID = %d, Command = %@", pid, command );
-        }
-        [ outputFile closeFile ];
-        
-        if ([ task terminationStatus ]==0 && ! taskKilled) {
-            returnString = AUTORELEASE([[NSString alloc] initWithData: outputData encoding: NSUTF8StringEncoding]);
+        else
+        {
+            ERROR_LOG(@"Task returned early. Exit code = %d, Command = %@", [task terminationStatus], scriptAndArgs );
         }
     }
-    else
-    {
-        ERROR_LOG(@"Task returned early. Exit code = %d, Command = %@", [task terminationStatus], command );
+    @catch (NSException *exception) {
+        ERROR_LOG(@"Command %@ does not exist", command);
     }
     return returnString;
 
