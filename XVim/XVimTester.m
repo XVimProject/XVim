c4ecc1e3e7445d2d1d8b47d2aa4188638e44794b
JugglerShu <shu@jugglershu.net>
2013-04-30 12:19:52 +0900

    Add comment "how to creat test cases"

--- XVim/XVimTester.m
+++ XVim/XVimTester.m
@@ -14,6 +14,56 @@
 #import "XVimKeyStroke.h"
 #import "XVimUtil.h"
 
+/**
+ * How to run test:
+ *
+ * 1 Write following line in .xvimrc (This makes Run Test menu command appeard)
+ *     set debug
+ *
+ * 2 Set forcus on text source view
+ *
+ * 3 Select menu [XVim]-[Run Test] and follow the dialog.
+ *
+ **/
+ 
+/**
+ * How to create test cases:
+ *
+ * 1. Create category for XVimTester (unless you can find proper file to write test case)
+ *    For example, create file with name "XVimTester+mytest.m" and write
+ *       #import "XVimTester.h"
+ *       @implementation XVimTester(mytest)
+ *       @end
+ *
+ *    (You do not need to create .h for the category)
+ *
+ * 2. Define method named "*_testcases" where * is wildcard. The method must return NSArray*.
+ *    For example 
+ *       - (NSArra*)mytest_testcases{ ... }
+ *
+ * 3. Create array of test cases and return it. A test case must be created with XVimMakeTestCase Macro.
+ *    For example
+ *       return [NSArray arrayWithObjects:
+ *                   XVimMakeTestCase("abc", 0, 0, "l", "abc", 1, 0),
+ *                   XVimMakeTestCase("abc", 0, 0, "x",  "bc", 0, 0),
+ *               nil];
+ *     
+ *    XVimMakeTestCase arguments are...
+ *     Initial text,
+ *     Initial selected range location,
+ *     Initial selected range length,
+ *     Vim command to test,
+ *     Expected result text,
+ *     Expected result selected range location,
+ *     Expected result selected range length
+ *
+ *    The first example above means
+ *     With the test "abc" and insertion point on "a" and input "l"
+ *     must result in with the uncahnged text with the cursor at "b"
+ *
+ * 
+ * Test cases you wrote automatically included and run.
+ **/
 
 @implementation XVimTester
 
