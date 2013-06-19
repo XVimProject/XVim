c0ea03dfaaa7626b4da7db640e8334d4f908d25d
JugglerShu <shu@jugglershu.net>
2013-06-19 23:14:22 +0900

    Added key mapping test for map, imap

--- XVim/Test/XVimTester+map.m
+++ XVim/Test/XVimTester+map.m
@@ -15,6 +15,16 @@
                              @"ggg hhh i_i\n"  // 24 28 32
                              @"    jjj kkk";   // 36 40 44
     
+    static NSString* imap_result1 = @"a;a def ccc\n"  // 0  4  8
+                                    @"ddd e-e fff\n"  // 12 16 20
+                                    @"ggg hhh i_i\n"  // 24 28 32
+                                    @"    jjj kkk";   // 36 40 44
+    
+    static NSString* imap_result2 = @"a;a abc ccc\n"  // 0  4  8
+                                    @"ddd e-e fff\n"  // 12 16 20
+                                    @"ggg hhh i_i\n"  // 24 28 32
+                                    @"    jjj kkk";   // 36 40 44
+    
     return [NSArray arrayWithObjects:
             // map
             // unmap
@@ -24,11 +34,24 @@
             XVimMakeTestCase(text2, 5,  0, @":map<SPACE>jj<SPACE>l<CR>jj", text2, 6, 0),
             XVimMakeTestCase(text2, 5,  0, @":mapclear<CR>jj", text2, 29, 0),
             
+            // remapping
+            // abc->def and defl->lll.  abc must resutls in lll
+            XVimMakeTestCase(text2, 5,  0, @":map<SPACE>abc<SPACE>def<CR>:map<SPACE>def<SPACE>lll<CR>abc", text2, 8, 0),
+            XVimMakeTestCase(text2, 5,  0, @":mapclear<CR>", text2, 5, 0), // Just for reset the previous mapping
             
             // noremap
+            // abc->lll (noremap) and lll->hhh.  abc must resutls in lll (not hhh)
+            XVimMakeTestCase(text2, 5,  0, @":noremap<SPACE>abc<SPACE>lll<CR>:map<SPACE>lll<SPACE>hhh<CR>abc", text2, 8, 0),
+            XVimMakeTestCase(text2, 5,  0, @":mapclear<CR>", text2, 5, 0), // Just for reset the previous mapping
             
             
             // imap
+            XVimMakeTestCase(text2, 4,  0, @":imap<SPACE>abc<SPACE>def<CR>xxxiabc<ESC>", imap_result1 , 6, 0),
+            XVimMakeTestCase(text2, 4,  0, @":iunmap<SPACE>abc<CR>xxxiabc<ESC>", imap_result2, 6, 0),
+            XVimMakeTestCase(text2, 4,  0, @":imap<SPACE>abc<SPACE>def<CR>:imapclear<CR>xxxiabc<ESC>", imap_result2 , 6, 0),
+            XVimMakeTestCase(text2, 4,  0, @":imap<SPACE>lll<SPACE>hhh<CR>lll", text2, 7, 0), // imap does not affect NORMAL mode
+            XVimMakeTestCase(text2, 4,  0, @":imapclear<CR>", text2, 4, 0),
+            
             // nmap
             // omap
             // vmap
