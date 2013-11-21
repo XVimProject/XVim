3afd25bf7f77991a2497c11071e43131fe747025
John AppleSeed <JTheAppleSeed@gmail.com>
2013-11-22 00:57:17 +0100

    Move the IDEWorkspaceWindowHook as a swizzle in XVimHookManager
    It's disabled by default because it only make sense for debugging
    purposes.

--- XVim/XVimHookManager.m
+++ XVim/XVimHookManager.m
@@ -6,19 +6,44 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import "Logger.h"
+#import "XVimView.h"
 #import "XVimHookManager.h"
 #import "IDEEditor+XVim.h"
 #import "IDEEditorArea+XVim.h"
-#import "IDEWorkspaceWindowHook.h"
 #import "DVTSourceTextScrollViewHook.h"
-#import "XVimView.h"
+#import "NSEvent+VimHelper.h"
+#import "NSObject+XVimAdditions.h"
+
+@implementation IDEWorkspaceWindow (XVim)
+
++ (void)xvim_initialize
+{
+#if 0 // Only useful for debugging purposes
+    if (self == [IDEWorkspaceWindow class]) {
+        [self xvim_swizzleInstanceMethod:@selector(sendEvent:)
+                                    with:@selector(xvim_sendEvent:)];
+    }
+#endif
+}
+
+- (void)xvim_sendEvent:(NSEvent *)event
+{
+    if (event.type == NSKeyDown) {
+        TRACE_LOG(@"Window:%p keyCode:%d characters:%@ charsIgnoreMod:%@ cASCII:%d",
+                  self, event.keyCode, event.characters, event.charactersIgnoringModifiers, event.unmodifiedKeyCode);
+    }
+    [self xvim_sendEvent:event];
+}
+
+@end
 
 @implementation XVimHookManager
 
 + (void)hookWhenPluginLoaded
 {
+    [IDEWorkspaceWindow xvim_initialize];
     [IDEEditorArea xvim_initialize];
-    [IDEWorkspaceWindowHook hook];
     [DVTSourceTextScrollViewHook hook];
     [IDEEditor xvim_initialize];
     [IDEComparisonEditor xvim_initialize];
