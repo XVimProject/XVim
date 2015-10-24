752c30f508ec80c24505f0fd4c55260074afe811
JugglerShu <shu@jugglershu.net>
2015-10-25 01:14:13 +0900

    Fix #846 In Playground you must use 'set blinkcursor' setting. Otherwise you can't see the cursor. This has been fixed once before in #622 but after that it returned back...

--- XVim/IDESourceCodeEditor+XVim.m
+++ XVim/IDESourceCodeEditor+XVim.m
@@ -15,6 +15,7 @@
 #import "XVim.h"
 #import "NSObject+XVimAdditions.h"
 #import "NSobject+ExtraData.h"
+#import <objc/runtime.h>
 
 @implementation IDESourceCodeEditor(XVim)
 + (void)xvim_initialize{
@@ -29,11 +30,20 @@
 
 - (id)xvim_initWithNibName:(NSString*)name bundle:(NSBundle*)bundle document:(IDEEditorDocument*)doc{
     id obj = [self xvim_initWithNibName:name bundle:bundle document:doc];
+    
     NSView* container = [[obj view] contentView];
+    DVTSourceTextScrollView* scrollView = [self mainScrollView];
+    
+    if ([obj isKindOfClass:NSClassFromString(@"IDEPlaygroundEditor")]) {
+        // IDEPlaygroundEditor have one more deeper container
+        // Should create new category for IDEPlaygroundEditor
+        NSViewController* controller = [[[obj view] contentView] viewController];
+        container = [[controller view] performSelector:@selector(contentView)];
+        scrollView = [controller performSelector:@selector(scrollView)];
+    }
     
     // Insert status line
-    if( nil != container ){
-        DVTSourceTextScrollView* scrollView = [self mainScrollView];
+    if( nil != container && nil != scrollView){
         [scrollView setTranslatesAutoresizingMaskIntoConstraints:NO]; // To use autolayout we need set this NO
         
         // Add status view
